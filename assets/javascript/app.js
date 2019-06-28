be262de7148ea25c75157bbacd94aeff47c5a2a8
Seth Randell <sethhrandell@gmail.com>
2019-06-28 17:29:30 -0400

    Added timer and funtions to reset questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,51 +15,59 @@ var questions = [
         corectAnwser: "Tilde"
     },
 ]
+var counter = 10;
 
-var interval = setInterval(function() {
-    counter--;
-    $("#counter").text(counter);
-    if (counter == 0) {
-       alert("Times up you")
-        clearInterval(interval);
-    }
-}, 1000);
+    // if (counter == 0) {
+    //    alert("Times up you")
+    //     clearInterval(interval);
+    // }
+
 
 var questionsList = [];
 var pastQuestions = [];
 var andomQuestion = [];
 
-// for(i = 0; i < questions.length; i++) {
-//     questionsList.push(questions[i].question + "");
-// }
-
 function random(){
 randomQuestion = questions[Math.floor(Math.random() * questions.length)];
 pastQuestion = randomQuestion.question;
 pastQuestions.push(pastQuestion + "");
-console.log(pastQuestion);
+// console.log(pastQuestion);
 }
 random();
-console.log(randomQuestion);
+// console.log(randomQuestion);
 
-// console.log(questions[1].question);
-// console.log(data.question1.anwsers.length);
 
-// Math.floor(Math.random) * 
+function generateDivs(){
+    $("#question").prepend("<h2 id='tmp'>" + randomQuestion.question + "</h2>");
+    for(i = 0; i< randomQuestion.anwsers.length; i++) {
+    $(".anwsers").prepend("<button id=" + "'" + randomQuestion.anwsers[i] + "'" + "type='button' class='btn btn-primary btn-lg btn-anwsers'>" + randomQuestion.anwsers[i] + "</button>");
+    }};
+
+function detachDivs(){
+    $("#tmp").detach();
+    $(".btn-anwsers").detach();
+};
+
 
 
 $("#startGame").click(function() {
+    setInterval(function() {
+        counter--;
+        $("#counter").text(counter);
+    }, 1000);
     $("#startGame").detach();
-    $("#question").append("<h2>" + randomQuestion.question + "</h2>");
-    // if(randomQuestion === questions[0].question {
-    for(i = 0; i< randomQuestion.anwsers.length; i++) {
-        $(".anwsers").append("<button id=" + "'" + randomQuestion.anwsers[i] + "'" + "type='button' class='btn btn-primary btn-lg btn-anwsers'>" + randomQuestion.anwsers[i] + "</button>");
-        }
+        generateDivs();
         $(".btn-anwsers").click(function() {
         console.log(this.id);
-        if(this.id === randomQuestion.corectAnwser) {
-            alert("You picked the right anwser")
+        if(this.id === randomQuestion.corectAnwser && counter > 0) {
+            alert("You picked the right anwser");
+            random();
+            detachDivs();
+            generateDivs();
+            console.log(randomQuestion);        
+            counter = 10;
         } else {
             alert("You picked the wronganwser");
         }});
+   
     });
