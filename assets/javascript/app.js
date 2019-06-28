3308435c1b8726f83b8d288e655121a89fa0c70b
Seth Randell <sethhrandell@gmail.com>
2019-06-28 16:30:34 -0400

    Added more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,59 @@
 
 
-var data = {
-    question1: {
+var questions = [
+    {
         question: "In the 'Road Runner and Coyote' cartoons, what famous sound does the Road Runner make?",
         anwsers: ["Ping! Ping!", "Beep! Beep!", "Aooga! Aooga!", "Vroom! Vroom!"],
         corectAnwser: "Beep! Beep!"
     },
-    question2: {
-        question: "In the 'Road Runner and Coyote' cartoons, what famous sound does the Road Runner make?",
-        anwsers: ["Ping! Ping!", "Beep! Beep!", "Aooga! Aooga!", "Vroom! Vroom!"],
-        corectAnwser: "Beep! Beep!"
-    }
+    {
+        question: "According to an Aesop's fable, you shouldn't count what before they are hatched?",
+        anwsers: ["Fingers", "Chickens", "Snakes", "Guppies"],
+        corectAnwser: "Chickens"
+    },
+    {
+        question: "Which of the following is the symbol sometimes placed over the 'n' in the Spanish language?",
+        anwsers: ["Tilde", "Umlaut", "Circumflex", "Cedilla"],
+        corectAnwser: "Tilde"
+    },
+]
+
+
+var questionsList = [];
+var pastQuestions = [];
+var andomQuestion = [];
+
+// for(i = 0; i < questions.length; i++) {
+//     questionsList.push(questions[i].question + "");
+// }
+
+function random(){
+randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+pastQuestion = randomQuestion.question;
+pastQuestions.push(pastQuestion + "");
+console.log(pastQuestion);
 }
+random();
+console.log(randomQuestion);
 
-var random =[];
-data.random
-console.log(data.question1.question);
-console.log(data.question1.anwsers.length);
+// console.log(questions[1].question);
+// console.log(data.question1.anwsers.length);
 
-
+// Math.floor(Math.random) * 
 
 
 $("#startGame").click(function() {
     $("#startGame").detach();
-    $("#question").append("<h2>" + data.question1.question + "</h2>");
-    
-    for(i = 0; i< data.question1.anwsers.length; i++) {
-        $(".anwsers").append("<button id=" + "'" + data.question1.anwsers[i] + "'" + "type='button' class='btn btn-primary btn-lg btn-anwsers'>" + data.question1.anwsers[i] + "</button>");
+    $("#question").append("<h2>" + randomQuestion.question + "</h2>");
+    // if(randomQuestion === questions[0].question {
+    for(i = 0; i< randomQuestion.anwsers.length; i++) {
+        $(".anwsers").append("<button id=" + "'" + randomQuestion.anwsers[i] + "'" + "type='button' class='btn btn-primary btn-lg btn-anwsers'>" + randomQuestion.anwsers[i] + "</button>");
         }
         $(".btn-anwsers").click(function() {
         console.log(this.id);
-        if(this.id === data.question1.corectAnwser) {
+        if(this.id === randomQuestion.corectAnwser) {
             alert("You picked the right anwser")
-        }
-        });
-        // if(this === data.question1.corectAnwser){
-        //     alert("You guessed the right anwser")
-        // }
-        // console.log(data.question1.anwsers[i]);
-});
+        } else {
+            alert("You picked the wronganwser");
+        }});
+    });
