f01473907f53497fbf4543de6357e8dd1e346bc2
Seth Randell <sethhrandell@gmail.com>
2019-07-01 16:20:15 -0400

    added more questions to the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,6 +34,41 @@ var questions = [
         url:"https://usatftw.files.wordpress.com/2016/10/priceisright.gif",
         fail: "https://media0.giphy.com/media/mxP1qg93p6vlK/giphy.gif?cid=790b76115d19f9203255323741e68548&rid=giphy.gif"
     },
+    {
+        question: "What year was the first syndicated version of the Price is Right air starring Bob Barker?",
+        anwsers: ["1972", "1956", "1985", "1990"],
+        corectAnwser: "1972",
+        url:"https://images-na.ssl-images-amazon.com/images/I/51L3m-uixeL._SX355_.jpg",
+        fail: "https://media0.giphy.com/media/mxP1qg93p6vlK/giphy.gif?cid=790b76115d19f9203255323741e68548&rid=giphy.gif"
+    },
+    {
+        question: "What were the models referred to as during Bob Barker's tenure on the show?",
+        anwsers: ["Bobs Babes", "Barkers Babes", "Barkers Beauties", "Bobs Beauties"],
+        corectAnwser: "Barkers Beauties",
+        url:"https://johnstodderinexile.files.wordpress.com/2006/11/barker-beauties.jpg",
+        fail: "https://media0.giphy.com/media/mxP1qg93p6vlK/giphy.gif?cid=790b76115d19f9203255323741e68548&rid=giphy.gif"
+    },
+    {
+        question: "How were the contestants picked to be on the show?",
+        anwsers: ["They were interviewed prior to that days taping", "By Random Drawing", "By answering 10 correct questions", "By winning a trial game"  ],
+        corectAnwser: "They were interviewed prior to that days taping",
+        url:"https://thenypost.files.wordpress.com/2013/08/stan_blits-300x200.jpeg",
+        fail: "https://media0.giphy.com/media/mxP1qg93p6vlK/giphy.gif?cid=790b76115d19f9203255323741e68548&rid=giphy.gif"
+    },
+    {
+        question: "What studio number has the show been taped from for its entire run?",
+        anwsers: ["Studio 33", "Studio 13 ", "Studio 24 ", "Studio 36"  ],
+        corectAnwser: "Studio 33",
+        url:"http://www.priceisright.com/wp-content/uploads/2014/09/11.jpg",
+        fail: "https://media0.giphy.com/media/mxP1qg93p6vlK/giphy.gif?cid=790b76115d19f9203255323741e68548&rid=giphy.gif"
+    },
+    {
+        question: "What was the most expensive prize ever given away on the show?",
+        anwsers: ["A 1962 Chevy Corvette", "A 1965 Harley Davidson", "A 2002 Streamline Sailboat", "A Tesla Roadster"],
+        corectAnwser: "A Tesla Roadster",
+        url:"https://i.ytimg.com/vi/eKoYSQ9ENiM/maxresdefault.jpg",
+        fail: "https://media0.giphy.com/media/mxP1qg93p6vlK/giphy.gif?cid=790b76115d19f9203255323741e68548&rid=giphy.gif"
+    },
 ]
 
 var numberOfQuestions = 0;
@@ -47,22 +82,17 @@ var pastQuestions = [];
 var randomQuestion = [];
 var audio = new Audio('assets/audio/theme.mp3');
 var horn = new Audio('assets/audio/looser.mp3');
-console.log(questions[numberOfQuestions].question);
-console.log(questions[numberOfQuestions].anwsers);
-console.log(questions[numberOfQuestions].corectAnwser);
 
 
 function random(){
 randomQuestion = questions[Math.floor(Math.random() * questions.length)];
 tmp = randomQuestion.question;
 pastQuestions.push(tmp + "");
-
 }
 
 function ticker() {
-    if (numberOfQuestions < 4) {
+    if (numberOfQuestions < 9) {
         numberOfQuestions++;
-        console.log(numberOfQuestions);
         $("#response").detach();
         $("#responseImg").detach();
         generateDivs();
@@ -105,13 +135,11 @@ function timesUp(){
     $("#notAnwsered").html("Not Anwsered: " + notAnwsered); 
     detachDivs();
     $(".response").prepend("<h2 id='response'>You took too long to anwser the questiong....." + "'" + questions[numberOfQuestions].corectAnwser + "'" + " was the correct anwser!!!</h2>");
-    // pause();
     setTimeout(ticker, 2000);
     detachDivs();
 }
 
 function wrongAnwser(){
-    // alert("You picked the wrong anwser");
     wrongAnwsers++;
     $("#losses").html("Wrong: " + wrongAnwsers); 
     detachDivs();
@@ -158,10 +186,7 @@ generateDivs();
 timerHolder();
 });
 
-
-
 $(".anwsers").on("click", ".pr", function() {
-        console.log(this.id);
         if (this.id !== questions[numberOfQuestions].corectAnwser && counter > 0){
             clearInterval(clock);
             wrongAnwser();
@@ -170,9 +195,7 @@ $(".anwsers").on("click", ".pr", function() {
             clearInterval(clock);
             rightAnwser();
             };   
-        
             });
-
 
 function endGame(){
   audio.pause();
@@ -185,11 +208,8 @@ function endGame(){
   $(".end").append("<h3 id'results' class='ending'>Not Anwsered: " + notAnwsered);
   $(".end").prepend("<img id='clap' class='ending' src='https://media0.giphy.com/media/CiZuWYvJ1hnoI/giphy.gif'></img>");
   $(".end").append("<button id='playAgain' type='button' class='pr'>Play Agian</button>");
-//   if(rightAnwser > wrongAnwsers){
-//     console.log("It should ofput the gif up");
-//     $(".end").append("<img src='https://media0.giphy.com/media/CiZuWYvJ1hnoI/giphy.gif'></img>");
-// }
-    if(wrongAnwsers === 3 || notAnwsered === 3){
+
+    if(wrongAnwsers < 5 || notAnwsered < 5){
         horn.play();
     }
   };
